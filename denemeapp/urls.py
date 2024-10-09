dd5644dfa2febcfafe823eddd199b600b88e8ac6
unknown <yusufzbayram@outlook.com>
2024-10-09 19:08:43 +0300

    241009

--- denemeapp/urls.py
+++ denemeapp/urls.py
@@ -6,8 +6,5 @@ App_name = 'denemeapp'
 
 
 urlpatterns = [
-    path("", views.index),
-    path("", views.index, name="index"),
-    path("blog/<int:id>", views.blog, name="blog"),
+    path("", views.MainView.as_view(), name="index"),
 ] 
-
--- denemeapp/views.py
+++ denemeapp/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Kullanıcıdan veri almak için form
 
-def index(request):
-    return render(request, "denemeapp/index.html")
 
-def blog(request, id):
-    return render(request, "denemeapp/blog.html",{
-        "id": id
-    })
+
+class MainView(TemplateView):
+    template_name = "denemeapp/index.html"

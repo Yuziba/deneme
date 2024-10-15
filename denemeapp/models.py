169b38583bf9f6609c11c55149f772198e10907f
unknown <yusufzbayram@outlook.com>
2024-10-15 20:04:27 +0300

    241015

--- denemeapp/models.py
+++ denemeapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-
+class AaModel(models.Model):
+    nick = models.CharField(max_length=50)
+    mesaj = models.CharField(max_length=150)
+    def __str__(self):
+        return f"isim : {self.nick} mesaj {self.mesaj}"
